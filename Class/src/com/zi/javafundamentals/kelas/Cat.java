19140a9fcf61982caee21c5dcfa741a6c3946f88
sifakhru <fakhru2904@gmail.com>
2021-08-03 22:35:29 +0700

    update
    class

---
+++ Class/src/com/zi/javafundamentals/kelas/Cat.java
@@ -0,0 +1,9 @@
+package com.zi.javafundamentals.kelas;
+
+public class Cat {
+    public static void main(String[] args) {
+        Cat = new Cat();
+        Cat.method();
+        // komenkomen
+    }
+}
