fdbc2e95d1b634cd265f000cc48ab755bb8f37a7
sifakhru <fakhru2904@gmail.com>
2021-08-12 11:58:45 +0700

    Update Interface

---
+++ Interface/src/com/zi/javafundamental/oop/Animal.java
@@ -0,0 +1,18 @@
+package com.zi.javafundamental.oop;
+
+/* Error
+
+public class Animal {
+
+    public void eat() {
+        System.out.println("Eating anything.");
+    }
+}
+
+ */
+
+public interface Animal {
+    // code convention
+    String RESPIRATION = "Oxygen";
+    void eat();
+}
